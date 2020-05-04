f9434ba8d4f9cf244867f9c6ba3b8409e8cce286
LilasAll <lilas.allard@gmail.com>
2020-05-04 11:35:37 +0200

    elastisearch

--- microservice-consumer/src/main/java/fr/formation/inti/data/kafka/repository/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.formation.inti.data.kafka.repository;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
-
-import fr.formation.inti.data.kafka.model.Price;
-
-public interface PriceRepository extends ElasticsearchCrudRepository<Price, Long> {
-
-}
