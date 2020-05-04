def00108beb91ec9cd8aa2c1e703c86e40db128d
PerrineLer <perrine.leroy76@gmail.com>
2020-05-04 11:24:34 +0200

    travail sur consumer et elastic

--- microservice-consumer/src/main/java/fr/formation/inti/data/kafka/message/Price.java
+++ microservice-consumer/src/main/java/fr/formation/inti/data/kafka/message/Price.java
@@ -1,10 +1,24 @@
 package fr.formation.inti.data.kafka.message;
 
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 import lombok.Data;
 
 @Data
 public class Price {
+	@Id
+	@Field(type = FieldType.Long)
+	private Long idPrix;
+	@Field(type = FieldType.Double)
+	private double montant;
+	@Field(type = FieldType.Boolean)
+	private boolean active;
+	@Field(type = FieldType.Keyword)
 	private String code;
-    private String context;
-    private java.util.Date date;
+	@Field(type = FieldType.Date)
+	private Date date;
 }
---
+++ microservice-consumer/src/main/java/fr/formation/inti/data/kafka/repository/PriceRepository.java
@@ -0,0 +1,9 @@
+package fr.formation.inti.data.kafka.repository;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
+
+import fr.formation.inti.data.kafka.message.Price;
+
+public interface PriceRepository extends ElasticsearchCrudRepository<Price, Long>{
+
+}
